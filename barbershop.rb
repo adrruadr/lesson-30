23a113af0eab46877b288e74f3be63886f861c18
shyssirixius <ivmikvik@gmail.com>
2016-08-11 16:25:14 +0300

    return barbershop from ignore ;)

---
+++ barbershop.rb
@@ -0,0 +1,50 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :datestamp, presence: true
+  validates :color, presence: true
+
+
+end
+
+class Barber < ActiveRecord::Base
+
+end
+
+before do 
+	@barbers = Barber.all 
+end
+
+get '/' do
+
+erb :index
+end
+
+get '/visit' do
+  @c = Client.new 
+  erb :visit
+
+end
+
+post '/visit' do
+
+  @c = Client.new params[:client]
+  if @c.save 
+    erb "Спасибо, вы записались!"
+  else
+    @error = @c.errors.full_messages.first
+    erb :visit
+  end
+
+
+  
+  end
+
